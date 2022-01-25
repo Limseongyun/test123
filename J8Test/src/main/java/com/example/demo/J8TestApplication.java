7e978caf7064fa5e914edc407cda0f5005e4facb
seongyun.lim <seongyun.lim@e4net.net>
2022-01-25 13:24:41 +0900

    test

---
+++ J8Test/src/main/java/com/example/demo/J8TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J8TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J8TestApplication.class, args);
+	}
+
+}
