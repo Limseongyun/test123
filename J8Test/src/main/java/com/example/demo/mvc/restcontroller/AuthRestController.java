56b6e84ea65624b9009fa28a79bd3cccb2ed3234
seongyun.lim <seongyun.lim@e4net.net>
2022-01-26 10:30:43 +0900

    가입

--- J8Test/src/main/java/com/example/demo/mvc/restcontroller/AuthRestController.java
+++ J8Test/src/main/java/com/example/demo/mvc/restcontroller/AuthRestController.java
@@ -1,12 +1,19 @@
 package com.example.demo.mvc.restcontroller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.cmm.code.ApiCd;
 import com.example.demo.cmm.rvo.RVO;
+import com.example.demo.mvc.model.dto.JoinDto;
+import com.example.demo.mvc.model.entity.Member;
+import com.example.demo.mvc.model.entity.MemberMoney;
 import com.example.demo.mvc.service.AuthService;
 
 @RestController
@@ -16,11 +23,29 @@ public class AuthRestController {
 	
 	
 	@GetMapping("/token")
-	public RVO<String> token(String id, String pw){
+	public RVO<String> token(HttpServletRequest request, String id, String pw){
 		return RVO.<String>builder()
 				.msg("JWT 토큰이 생성되었습니다.")
 				.code(ApiCd.NORMAL)
-				.data(authService.gettoken(id, pw))
+				.data(authService.gettoken(id, pw, request.getRemoteAddr()))
+				.build();
+	}
+	
+	@PostMapping("/sellerJoin")
+	public RVO<Member> sellerJoin(@RequestBody JoinDto joinDto){
+		return RVO.<Member>builder()
+				.msg("판매자 가입 되었습니다.")
+				.code(ApiCd.NORMAL)
+				.data(authService.sellerJoin(joinDto))
+				.build();
+	}
+	
+	@PostMapping("/userJoin")
+	public RVO<MemberMoney> userJoin(@RequestBody JoinDto joinDto){
+		return RVO.<MemberMoney>builder()
+				.msg("사용자 가입 되었습니다.")
+				.code(ApiCd.NORMAL)
+				.data(authService.userJoin(joinDto))
 				.build();
 	}
 
--- J8Test/src/main/java/com/example/demo/mvc/service/AuthService.java
+++ J8Test/src/main/java/com/example/demo/mvc/service/AuthService.java
@@ -4,23 +4,34 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.cmm.code.Cd;
+import com.example.demo.cmm.utils.EntityUtil;
 import com.example.demo.cmm.utils.JwtTokenProvider;
+import com.example.demo.mvc.model.dto.JoinDto;
 import com.example.demo.mvc.model.entity.Member;
+import com.example.demo.mvc.model.entity.MemberLoginHst;
+import com.example.demo.mvc.model.entity.MemberMoney;
 import com.example.demo.mvc.model.entity.QMember;
+import com.example.demo.mvc.repository.MemberLoginHstRepo;
+import com.example.demo.mvc.repository.MemberMoneyRepo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Transactional
 @Slf4j
 public class AuthService {
 	@Autowired private PasswordEncoder pe;
 	@Autowired private JPAQueryFactory qf;
 	@Autowired private ModelMapper mm;
+	@Autowired private MemberMoneyRepo mmRepo;
+	@Autowired private EntityUtil eu;
+	@Autowired private MemberLoginHstRepo memLoginHstRepo;
 	
-	public String gettoken(String id, String pw) {
+	public String gettoken(String id, String pw, String ip) {
 		QMember qmem = QMember.member;
 		Member mem = qf.selectFrom(qmem).where(qmem.membId.eq(id)).fetchOne();
 		
@@ -34,6 +45,40 @@ public class AuthService {
 		if(! Cd.MEMBER_STTUS_OK.equals(mem.getMembSttusCd().getCodeValue())) {
 			throw new RuntimeException("유저 상태가 정상이 아닙니다.");
 		}
+		MemberLoginHst mlg = new MemberLoginHst();
+		mlg.setConnectIp(ip);
+		mlg.setMemberSn(mem);
+		memLoginHstRepo.save(mlg);
 		return JwtTokenProvider.generateToken(id, pw, mem.getMembCls().getCodeValue());
+	}
+	
+	public MemberMoney sellerJoin(JoinDto joinDto) {
+		QMember qmem = QMember.member;
+		
+		Member memChk = qf.selectFrom(qmem).where(qmem.membId.eq(joinDto.getMembId())).fetchOne();
+		if(memChk != null) {
+			throw new RuntimeException("이미 존재하는 아이디 입니다.");
+		}
+		MemberMoney newMem = mm.map(joinDto, MemberMoney.class);
+		newMem.setMoneyBlce(0L);
+		newMem.setMembPw(pe.encode(newMem.getPassword()));
+		newMem.setMembCls(eu.getMemberTyCmm(Cd.MEMBER_TY_SELLER));
+		newMem.setMembSttusCd(eu.getMemberSttusCmm(Cd.MEMBER_STTUS_OK));
+		return mmRepo.save(newMem);
+	}
+	
+	public MemberMoney userJoin(JoinDto joinDto) {
+		QMember qmem = QMember.member;
+		
+		Member memChk = qf.selectFrom(qmem).where(qmem.membId.eq(joinDto.getMembId())).fetchOne();
+		if(memChk != null) {
+			throw new RuntimeException("이미 존재하는 아이디 입니다.");
+		}
+		MemberMoney newMem = mm.map(joinDto, MemberMoney.class);
+		newMem.setMoneyBlce(0L);
+		newMem.setMembPw(pe.encode(newMem.getPassword()));
+		newMem.setMembCls(eu.getMemberTyCmm(Cd.MEMBER_TY_USER));
+		newMem.setMembSttusCd(eu.getMemberSttusCmm(Cd.MEMBER_STTUS_OK));
+		return mmRepo.save(newMem);
 	}
 }
