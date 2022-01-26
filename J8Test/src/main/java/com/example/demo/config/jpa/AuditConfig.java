877a6c1078915c02dd78aa3ef5cbb9b8591a37b3
seongyun.lim <seongyun.lim@e4net.net>
2022-01-26 12:45:57 +0900

    auditconfig

--- J8Test/src/main/java/com/example/demo/config/jpa/AuditConfig.java
+++ J8Test/src/main/java/com/example/demo/config/jpa/AuditConfig.java
@@ -5,9 +5,10 @@ import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import com.example.demo.mvc.model.entity.Member;
-
+@Component
 public class AuditConfig implements AuditorAware<Long>{
 
 	@Override
