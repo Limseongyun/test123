b6be751f04c856ac8bab050e71bab8b5ada05d1c
seongyun.lim <seongyun.lim@e4net.net>
2022-01-26 10:51:52 +0900

    audit config setting

--- J8Test/src/main/java/com/example/demo/config/jpa/AuditConfig.java
+++ J8Test/src/main/java/com/example/demo/config/jpa/AuditConfig.java
@@ -6,14 +6,17 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.example.demo.mvc.model.entity.Member;
+
 public class AuditConfig implements AuditorAware<Long>{
 
 	@Override
 	public Optional<Long> getCurrentAuditor() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Member mem = (Member) auth.getPrincipal();
 		if(auth == null || ! auth.isAuthenticated()) return null;
 		if("anonymousUser".equals(auth.getPrincipal())) return null;
-		String sn = String.valueOf(auth.getPrincipal());
+		String sn = String.valueOf(mem.getMemberSn());
 		return Optional.of(Long.parseLong(sn));
 	}
 
